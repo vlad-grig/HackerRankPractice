f48a59dc6bcedfe919cd15f71c28c1d6bf8902fd
Vlad Grigore <vladyg1986@gmail.com>
2021-03-22 22:39:30 +0200

    birthday candles second commit

--- src/com/company/BirthdayCandles.java
+++ src/com/company/BirthdayCandles.java
@@ -7,18 +7,25 @@ public class BirthdayCandles {
 
         List <Integer> candles = new ArrayList <>();
         candles.add(4);
-        candles.add(1);
+        candles.add(3);
         candles.add(2);
         candles.add(4);
         candles.add(1);
         candles.add(5);
         candles.add(2);
-        candles.add(4);
-        candles.add(4);
+        candles.add(1);
+        candles.add(3);
+        candles.add(3);
+        candles.add(3);
+        candles.add(3);
         int cakeCandles = birthdayCakeCandles(candles);
         System.out.println(cakeCandles);
+
+        int candles2 = birthdayCakeCandles2(candles);
+        System.out.println(candles2);
     }
 
+    //this function is faster but not enough faster for HackerRank(only 80%)
     static int birthdayCakeCandles(List <Integer> candles) {
         int numberOfCandles = 0;
         Set <Integer> setList = new HashSet <>(candles);
@@ -34,5 +41,23 @@ public class BirthdayCandles {
             }
         }
         return numberOfCandles;
+    }
+
+    //this function is lower than the first one
+    static int birthdayCakeCandles2(List <Integer> candles) {
+        int max = 0;
+        for(int i = 0; i < candles.size(); i++) {
+            int number = candles.get(i);
+            int count = 0;
+            for(int currentElement : candles) {
+                if (number == currentElement) {
+                    count++;
+                    if (count > max) {
+                        max = count;
+                    }
+                }
+            }
+        }
+        return max;
     }
 }
